4b4acfb0be9c68ac2094df05e27c8f273e9437ab
Jean-baptiste Olsommer <jean-baptisteolsommer@mac-mini-de-jean-baptiste.home>
2023-12-01 10:35:22 +0100

    first commit

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,10 +1,10 @@
 import { AppConfig } from '../utils/AppConfig';
 
-type ILogoProps = {
-  xl?: boolean;
-};
+// type ILogoProps = {
+//   xl?: boolean;
+// };
 
-const Logo = (props: ILogoProps) => {
+const Logo = () => {
   // const size = props.xl ? '44' : '32';
   // const fontStyle = props.xl
   //   ? 'font-semibold text-3xl'
